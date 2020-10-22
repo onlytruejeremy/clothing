c5c7c82192abf211a160398e61f7e88b7c83a454
onlytruejeremy <jeremydev2020@gmail.com>
2020-10-22 12:26:11 -0700

    Updated Menu Items

--- src/components/menu-item/menu-item.jsx
+++ src/components/menu-item/menu-item.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import {withRouter} from "react-router-dom"
 import "./menu-item.styles.scss"
-function MenuItem({title, imageUrl, size}) {
+function MenuItem({title, imageUrl, size, history, linkUrl, match}) {
     return (
-    <div className={`${size} menu-item`}>
+    <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
         <div className="background-image" style={{
         backgroundImage: `url(${imageUrl})`
     }}>
@@ -18,4 +19,6 @@ function MenuItem({title, imageUrl, size}) {
     )
 }
 
-export default MenuItem;
+export default withRouter(MenuItem);
+
+
